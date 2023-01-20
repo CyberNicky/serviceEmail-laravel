4d9f075861ab5679b95e9812666a687d443dd126
Monique Campos <97682461+CyberNicky@users.noreply.github.com>
2023-01-20 08:59:21 -0300

    Update controller

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -1,72 +1,50 @@
 <?php
- 
+
 namespace App\Http\Controllers;
- 
+
 use App\Http\Controllers\Controller;
 use App\Mail\OrderShipped;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\App;
- 
+use Illuminate\Support\Facades\DB;
+
 class EmailController extends Controller
 {
-    /**
-     * Ship the given order.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-        
-    {
-        
- 
-        // Ship the order...
-        Mail::to($request->user())->send(new OrderShipped());
- 
-        // Mail::to($request->user())
-        // ->cc($moreUsers)
-        // ->send(new OrderShipped());
+	/**
+	 * Ship the given order.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(Request $request)
 
-        foreach (['nickyalbuquerque2008@gmail.com', 'nickyalbuquerque2008@gmail.com'] as $recipient) {
-            dd("teste");
-            Mail::to($recipient)->send(new Shipped());
-            // Mail::mailer('postmark')
-            // ->to($request->user())
-            // ->send(new OrderShipped());
-            // Mail::to($request->user())
-            // ->cc($moreUsers)
-            // ->bcc($evenMoreUsers)
-            // ->queue(new OrderShipped());
-            // Mail::to($request->user())
-            // ->cc($moreUsers)
-            // ->bcc($evenMoreUsers)
-            // ->later(now()->addMinutes(10), new OrderShipped());
-            // $message = (new OrderShipped())
-            //     ->onConnection('sqs')
-            //     ->onQueue('emails');
- 
-            // Mail::to($request->user())
-            // ->cc($moreUsers)
-            // ->bcc($evenMoreUsers)
-            // ->queue($message);
-            // Mail::to($request->user())->send(
-            //     (new OrderShipped())->afterCommit()
-            // );
-            // Mail::to($request->user())->locale('es')->send(
-            //     new OrderShipped()
-            // );
-            
-        $environment = App::environment();
-        if (App::environment('local')) {
-            // The environment is local
-        }
-         
-        if (App::environment(['local', 'staging'])) {
-            // The environment is either local OR staging...
-        }
-    }
-
-    
-}
+	{
+		$emails = DB::table('tb_emails_envio')
+			->select(
+				'ID_EMAILS_ENVIO',
+				'EMAIL_DESTINO',
+				'CORPO_EMAIL',
+				'IND_ENVIADO',
+				'ASSUNTO'
+			)
+			->where('IND_ENVIADO', 'N')
+			->where('ID_EMAILS_ENVIO', '5')
+			->get();
+		foreach ($emails as $recipient) {
+			$recipient->id_emails_envio;
+			$recipient->email_destino;
+			$recipient->corpo_email;
+			$recipient->ind_enviado;
+			$recipient->assunto;
+			Mail::send([], [], function ($message) use ($recipient) {
+				$message->to($recipient->email_destino);
+				$message->subject($recipient->assunto);
+				$message->html($recipient->corpo_email, 'text/html');
+			});
+			DB::table('tb_emails_envio')
+				->where('id')
+				->update(['IND_ENVIADO' => 'S']);
+		}
+	}
 }
